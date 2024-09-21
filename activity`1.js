95dca802b2d5b41042a2bd41b5de82a8379722e3
unknown <zedlavhtide@gamil.com>
2024-09-21 19:27:42 +0800

    first commit

---
+++ activity`1.js
@@ -0,0 +1,56 @@
+// PERSONA 1
+let P1fName ="Maria ";
+let P1mName="Delos Santos ";
+let P1lName ="LasMarias ";
+let P1age = 25;
+let P1addressed = "Upper Session Road,Baguio City,Buenget";
+let fullName =  P1fName + P1mName + P1lName
+let P1Name = P1lName + P1fName + P1mName 
+
+
+console.log(fullName)
+console.log(P1age)
+console.log(P1addressed)
+
+
+
+
+
+//personal 2
+let P2fName ="Juan ";
+let P2mName="Gamoso ";
+let P2lName ="Dela Cruz";
+let P2Age = 28;
+let P2addressed = "San Nicolas,Candon City,Ilocos Sur";
+let fulName =  P2fName + P2mName + P2lName
+let P2Name = P2lName + P2fName + P2mName 
+
+
+console.log(fulName)
+console.log(P2Age)
+console.log(P2addressed)
+
+//SPLIT
+let splitArray = P1Name.split("") + P2Name.split("");
+console.log(splitArray)
+
+//LENGTH
+console.log(fullName.length);
+console.log(fulName.length);
+console.log(P1addressed.length);
+console.log(P2addressed.length);
+
+//OPERATIONS
+age1 = 25;
+age2 = 28;
+length1 = 29
+length2 = 21
+add1 = 38;
+add2 = 34;
+console.log(age1 + age2 - length1 * length2)
+console.log(add1 ** add2)
+
+
+
+
+
